218cc052fc0128bf722009c8ea53326bbc324f21
VitoLouis <Vitolouis22@gmail.com>
2024-04-23 22:43:00 +0700

    ke ganti semua ke php

---
+++ html/menu.php
@@ -0,0 +1,92 @@
+<div class="menu">
+        <ul class="list">
+          <li class="header">MAIN NAVIGATION</li>
+          <li class="active open">
+            <a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-home"></i><span>Dashboard</span></a>
+            <ul class="ml-menu">
+              <li class="active"><a href="index.php">Main Dashboard</a></li>
+              <li><a href="ec-dashboard.php">Ecommerce Dashboard</a></li>
+            </ul>
+          </li>
+          <li>
+        
+          </li>
+          <li>
+            <a href="mail-inbox.html"><i class="zmdi zmdi-email"></i><span>Inbox</span> </a>
+          </li>
+          <li>
+            
+          </li>
+          <li>
+          <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-shopping-cart"></i><span>Ecommerce</span> </a>
+            <ul class="ml-menu">
+            <li><a href="ec-dashboard.php">Dashboard</a></li>
+              <li><a href="ec-product.php">Product</a></li>
+              <li><a href="ec-product-List.php">Shopping Cart</a></li>
+              <li><a href="ec-product-detail.php">Product detail</a></li>
+            </ul>
+          </li>
+          <li>
+           
+          </li>
+          <li>
+           
+          </li>
+          <li>
+            <a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-grid"></i><span>Tables</span> </a>
+            <ul class="ml-menu">
+              <li><a href="normal-tables.html">Normal Tables</a></li>
+              <li><a href="jquery-datatable.html">Jquery Datatables</a></li>
+              <li><a href="editable-table.html">Editable Tables</a></li>
+              <li><a href="footable.html">Foo Tables</a></li>
+              <li><a href="table-color.html">Tables Color</a></li>
+            </ul>
+          </li>
+          <li>
+           
+          </li>
+          <li>
+            
+          </li>
+          <li>
+            <a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-lock"></i><span>Authentication</span> </a>
+            <ul class="ml-menu">
+              <li><a href="sign-in.php">Sign In</a></li>
+              <li><a href="sign-up.html">Sign Up</a></li>
+              <li><a href="forgot-password.html">Forgot Password</a></li>
+              <li><a href="404.html">Page 404</a></li>
+              <li><a href="500.html">Page 500</a></li>
+              <li><a href="page-offline.html">Page Offline</a></li>
+              <li><a href="locked.html">Locked Screen</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-copy"></i><span>Extra Pages</span> </a>
+            <ul class="ml-menu">
+              <li><a href="blank.html">Blank Page</a></li>
+              <li><a href="events.html">Calendar</a></li>
+              <li><a href="profile.html">Profile</a></li>
+              <li><a href="timeline.html">Timeline</a></li>
+              <li><a href="chat.html">Chat</a></li>
+              <li><a href="contact.html">Contact list</a></li>
+              <li><a href="pricing.html">Pricing</a></li>
+              <li><a href="invoices.html">Invoices</a></li>
+              <li><a href="search-results.html">Search Results</a></li>
+            </ul>
+          </li>
+          <li>
+          
+          <li class="header">LABELS</li>
+          <li>
+            <a href="javascript:void(0);"><i class="zmdi zmdi-chart-donut col-red"></i><span>Important</span> </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);"><i class="zmdi zmdi-chart-donut col-amber"></i><span>Warning</span> </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);"><i class="zmdi zmdi-chart-donut col-blue"></i><span>Information</span> </a>
+          </li>
+        </ul>
+      </div>
+      <!-- #Menu -->
+    </aside>
